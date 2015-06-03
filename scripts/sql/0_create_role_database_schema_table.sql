bd345a180142356616114b061bc126b316075cfc
Diego Nobre <contato@diegonobre.net>
2015-06-03 01:08:23 -0300

    Update and rename 0_create_table.sql to 0_create_role_database_schema_table.sql

--- scripts/sql/0_create_role_database_schema_table.sql
+++ scripts/sql/0_create_role_database_schema_table.sql
@@ -1,3 +1,24 @@
+CREATE ROLE morpheus LOGIN
+   ENCRYPTED PASSWORD 'pilulavermelhar'
+   SUPERUSER INHERIT CREATEDB CREATEROLE REPLICATION;
+
+CREATE DATABASE controlesocial
+  WITH OWNER = morpheus
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Portuguese_Brazil.1252'
+       LC_CTYPE = 'Portuguese_Brazil.1252'
+       CONNECTION LIMIT = -1;
+
+CREATE SCHEMA tse
+  AUTHORIZATION morpheus;
+
+COMMENT ON SCHEMA tse
+  IS 'Repositório de dados do Tribunal Superior Eleitoral
+- Prestação de contas de campanha;
+- Votação nominal por UF e município;
+- Filiações partidárias;';
+
 DROP table IF EXISTS despesas_candidatos;
 create table despesas_candidatos (
     "Cód. Eleição" TEXT, "Desc. Eleição" TEXT, "Data e hora" TEXT, "CNPJ Prestador Conta" TEXT, "Sequencial Candidato" TEXT, "UF" TEXT, "Sigla  Partido" TEXT, "Número candidato" TEXT, "Cargo" TEXT, "Nome candidato" TEXT, "CPF do candidato" TEXT, "Tipo do documento" TEXT, "Número do documento" TEXT, "CPF/CNPJ do fornecedor" TEXT, "Nome do fornecedor" TEXT, "Nome do fornecedor (Receita Federal)" TEXT, "Cod setor econômico do doador" TEXT, "Setor econômico do fornecedor" TEXT, "Data da despesa" TEXT, "Valor despesa" TEXT, "Tipo despesa" TEXT, "Descriçao da despesa" TEXT
