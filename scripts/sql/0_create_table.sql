30d8dba566971370492b44de3787db071a6749b5
Diego Nobre <contato@diegonobre.net>
2015-06-03 00:47:12 -0300

    Create 0_create_table.sql

---
+++ scripts/sql/0_create_table.sql
@@ -0,0 +1,4 @@
+DROP table IF EXISTS despesas_candidatos;
+create table despesas_candidatos (
+    "Cód. Eleição" TEXT, "Desc. Eleição" TEXT, "Data e hora" TEXT, "CNPJ Prestador Conta" TEXT, "Sequencial Candidato" TEXT, "UF" TEXT, "Sigla  Partido" TEXT, "Número candidato" TEXT, "Cargo" TEXT, "Nome candidato" TEXT, "CPF do candidato" TEXT, "Tipo do documento" TEXT, "Número do documento" TEXT, "CPF/CNPJ do fornecedor" TEXT, "Nome do fornecedor" TEXT, "Nome do fornecedor (Receita Federal)" TEXT, "Cod setor econômico do doador" TEXT, "Setor econômico do fornecedor" TEXT, "Data da despesa" TEXT, "Valor despesa" TEXT, "Tipo despesa" TEXT, "Descriçao da despesa" TEXT
+);
