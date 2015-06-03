231223fa749ef11293bf3ddacfe46557c3a4136c
Diego Nobre <contato@diegonobre.net>
2015-06-03 01:10:31 -0300

    Update tomando-a-pilula-vermelha.sh

--- scripts/tomando-a-pilula-vermelha.sh
+++ scripts/tomando-a-pilula-vermelha.sh
@@ -36,54 +36,7 @@ unzip prestacao_final_2014.zip
 # Importação para banco PostgreSQL #
 ####################################
 echo 'Criando usuário do banco PostgreSQL: morpheus com a senha "pilulavermelha" ...';
-psql -h localhost -p 5432 -U morpheus -c
-"CREATE ROLE morpheus LOGIN
-   ENCRYPTED PASSWORD 'pilulavermelhar'
-   SUPERUSER INHERIT CREATEDB CREATEROLE REPLICATION;"
+git clone https://github.com/controlesocial/repositorio-de-dados.git /home/$USER/controlesocial
+cd /home/$USER/controlesocial/
 
-echo 'Criando banco de dados: controlesocial ...';
-psql -h localhost -p 5432 -U morpheus -c
-"CREATE DATABASE controlesocial
-  WITH OWNER = morpheus
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       LC_COLLATE = 'Portuguese_Brazil.1252'
-       LC_CTYPE = 'Portuguese_Brazil.1252'
-       CONNECTION LIMIT = -1;"
-
-echo 'Criando tabela despesas_candidatos ...';
-psql -h localhost -p 5432 -U morpheus -c
-'
-create table despesas_candidatos (
-"Cód. Eleição" TEXT, "Desc. Eleição" TEXT, "Data e hora" TEXT, "CNPJ Prestador Conta" TEXT, "Sequencial Candidato" TEXT, "UF" TEXT, "Sigla  Partido" TEXT, "Número candidato" TEXT, "Cargo" TEXT, "Nome candidato" TEXT, "CPF do candidato" TEXT, "Tipo do documento" TEXT, "Número do documento" TEXT, "CPF/CNPJ do fornecedor" TEXT, "Nome do fornecedor" TEXT, "Nome do fornecedor (Receita Federal)" TEXT, "Cod setor econômico do doador" TEXT, "Setor econômico do fornecedor" TEXT, "Data da despesa" TEXT, "Valor despesa" TEXT, "Tipo despesa" TEXT, "Descriçao da despesa" TEXT
-);
-'
-
-echo 'Iniciando cópia dos arquivos de 2014 ...'
-psql -h localhost -p 5432 -U morpheus -c "
-DO $$
-DECLARE
-    uf VARCHAR(2);
-    arrayUFs VARCHAR(2)[] := ARRAY['AC', 'AL', 'AM', 'AP', 'BA', 'BR', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
-BEGIN
-
-    RAISE NOTICE '%', arrayUFs;
-
-    FOREACH uf IN ARRAY arrayUFs
-    LOOP
-        RAISE NOTICE '%', uf;
-        EXECUTE 'COPY despesas_candidatos FROM ''C:\Program Files\PostgreSQL\9.4\data\pg_log\prestacao_final_2014\despesas_candidatos_2014_'||uf||'.txt'' WITH CSV HEADER DELIMITER '';'';';
-    END LOOP;
-END; $$
-"
-psql -h localhost -p 5432 -U morpheus -c '
-ALTER TABLE despesas_candidatos ALTER COLUMN "Cód. Eleição" TYPE INTEGER USING (("Cód. Eleição")::INTEGER);
-ALTER TABLE despesas_candidatos ALTER COLUMN "Data e hora" TYPE TIMESTAMP WITHOUT TIME ZONE USING (("Data e hora")::TIMESTAMP WITHOUT TIME ZONE);
-ALTER TABLE despesas_candidatos ALTER COLUMN "Número candidato" TYPE INTEGER USING (("Número candidato")::INTEGER);
-
-UPDATE despesas_candidatos SET "Data da despesa" = REPLACE( "Data da despesa", '00:00:00', '');
-ALTER TABLE despesas_candidatos ALTER COLUMN "Data da despesa" TYPE DATE USING (("Data da despesa")::DATE);
-
-UPDATE despesas_candidatos SET "Valor despesa" = REPLACE( "Valor despesa", ',', '.');
-ALTER TABLE despesas_candidatos ALTER COLUMN "Valor despesa" TYPE NUMERIC(12,2) USING (("Valor despesa")::NUMERIC(12,2));
-'
+psql -h localhost -p 5432 -U morpheus -f "./repositorio-de-dados/scripts/sql/0_create_role_database_schema_table.sql"
